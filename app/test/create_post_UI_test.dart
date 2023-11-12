de7d2c5a75f1ebd902da8ad48837da5613faf44c
Ishita Gupta <ishita1809@gmail.com>
2023-11-11 16:44:03 -0800

    Modified create-post UI test; fails right now

--- app/test/create_post_UI_test.dart
+++ app/test/create_post_UI_test.dart
@@ -10,15 +10,25 @@ void main(){
     ));
       
     // Verify that the Save button text is rendered.
-    expect(find.text('Save'), findsOneWidget);
+    expect(find.text('Save'), findsOneWidget, reason: "Save text not found");
     // Verify that the Cancel button icon is rendered.
-    expect(find.byIcon(Icons.close), findsOneWidget);
+    expect(find.byIcon(Icons.close), findsOneWidget, reason: "Cancel/Close icon not found");
 
     // Verify that both buttons are present
-    expect(find.byType(ElevatedButton), findsOneWidget);
-    expect(find.byType(IconButton), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsOneWidget, reason: "Save button not found");
+    expect(find.byType(IconButton), findsOneWidget, reason: "Cancel button not found");
 
-    //Verify the text input fields for title, description, alt text, and pickup instructions are present
-    expect(find.byType(TextField), findsNWidgets(4));
+    // Verify that the text input fields for title, description, alt text, search bars for allergens and tags, and pickup instructions are present
+    expect(find.byType(TextField), findsWidgets, reason: "1 or more text fields are missing");
+
+    // Verify that the search bar text is present for 2 widgets
+    expect(find.widgetWithText(TextField, 'Search'), findsNWidgets(2), reason: "1 or more search bar is missing");
+
+    // Verify that the TimePicker is present.
+    expect(find.byType(TimePickerDialog), findsOneWidget, reason: "Time picker is not found");
+
+    // Verify that the TimePicker is present.
+    expect(find.byType(DatePickerDialog), findsOneWidget, reason: "Date picker is not found");
+
   });
 }
