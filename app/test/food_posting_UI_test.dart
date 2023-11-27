5c1eeec75ae395ae7142238169ed7ae0cf2f08f1
Jayati <jae186@gmail.com>
2023-11-26 23:10:06 -0800

    continuing to fix the UI and updating test sim

--- app/test/food_posting_UI_test.dart
+++ app/test/food_posting_UI_test.dart
@@ -1,21 +1,22 @@
+import 'package:FoodHood/Screens/food_posting.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:FoodHood/Screens/food_posting.dart';
 
 
 void main() {
   testWidgets('FoodPosting UI Test', (WidgetTester tester) async {
-    // Builds page
-    await tester.pumpWidget(FoodPosting());
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(FoodPostingBig());
 
-    // Verify that button is rendering correctly
-    expect(find.text('Reserve'), findsOneWidget);
+    // Verify that the back button is present.
+    expect(find.byIcon(CupertinoIcons.back), findsOneWidget);
 
-    // Verify that there is a heart button
-    expect(find.byType(FloatingActionButton), findsOneWidget);
+    // Verify that the heart and share buttons are present.
+    expect(find.byIcon(CupertinoIcons.heart), findsOneWidget);
+    expect(find.byIcon(CupertinoIcons.share), findsOneWidget);
 
-    /// Verify there are tags
-    expect(find.byType(ListTile), findsNWidgets(8));
-
+    // Verify that the reserve button is present.
+    expect(find.byType(CupertinoButton), findsWidgets);
   });
 }
