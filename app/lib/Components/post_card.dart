600a47615c1a50b0587d8de20a1d9c5b884b1c18
Dhruv Bihani <dhruvbihani@gmail.com>
2023-11-29 21:51:20 -0800

    all the posts cards are now clickable

--- app/lib/Components/post_card.dart
+++ app/lib/Components/post_card.dart
@@ -30,20 +30,27 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16), // Add padding here
-      child: Center(
-        child: Container(
-          width: 382,
-          height: 220,
-          decoration: _buildBoxDecoration(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Spacer(),
-              _buildTitleSection(),
-              _buildTagSection(),
-              _buildOrderInfoSection(),
-            ],
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: CupertinoButton(
+        padding:
+            EdgeInsets.zero, // Removes default padding from CupertinoButton
+        onPressed: () {
+          print('Post card was clicked');
+        },
+        child: Center(
+          child: Container(
+            width: 382,
+            height: 220,
+            decoration: _buildBoxDecoration(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Spacer(),
+                _buildTitleSection(),
+                _buildTagSection(),
+                _buildOrderInfoSection(),
+              ],
+            ),
           ),
         ),
       ),
