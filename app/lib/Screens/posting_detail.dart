b0cc5279edf7b7acd45810c965058b0cfea9b455
Jayati <jae186@gmail.com>
2023-12-01 17:00:13 -0800

    finished UI with reserved boolean

--- app/lib/Screens/posting_detail.dart
+++ app/lib/Screens/posting_detail.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:feather_icons/feather_icons.dart';
 
+//import status
+
 class PostDetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,7 +21,7 @@ class PostDetailView extends StatelessWidget {
                 size: 22, color: CupertinoColors.systemRed),
             SizedBox(width: 15),
             Icon(FeatherIcons.share,
-                size: 22, color: CupertinoColors.systemBlue) // Second item
+                size: 22, color: CupertinoColors.systemBlue)
           ],
         ),
       ),
@@ -48,7 +50,7 @@ class PostDetailView extends StatelessWidget {
                           ),
                         ),
                       ),
-                      AvailabilityIndicator(),
+                      AvailabilityIndicator(isReserved: true),
                     ],
                   ),
                   const SizedBox(height: 8),
@@ -62,10 +64,15 @@ class PostDetailView extends StatelessWidget {
                   const SizedBox(height: 8),
                   InfoRow(),
                   const SizedBox(height: 16),
+                  InfoCardsRow(),
                   PickupInformation(),
                   const SizedBox(height: 16),
                   AllergensSection(),
-                  ReserveButton(),
+                  Center(
+                    child: ReserveButton(
+                      isReserved: false,
+                    ),
+                  )
                 ],
               ),
             ),
@@ -77,11 +84,23 @@ class PostDetailView extends StatelessWidget {
 }
 
 class AvailabilityIndicator extends StatelessWidget {
+  final bool isReserved;
+  AvailabilityIndicator({required this.isReserved});
+
   @override
   Widget build(BuildContext context) {
+    Color indicatorColor = isReserved
+        ? CupertinoColors.systemRed.withOpacity(0.2)
+        : CupertinoColors.activeGreen.withOpacity(0.2);
+
+    Color circleColor =
+        isReserved ? CupertinoColors.systemRed : CupertinoColors.activeGreen;
+
+    String statusText = isReserved ? 'Reserved' : 'Available';
+
     return Container(
       decoration: BoxDecoration(
-        color: CupertinoColors.activeGreen.withOpacity(0.2),
+        color: indicatorColor,
         borderRadius: BorderRadius.circular(20),
       ),
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -91,14 +110,14 @@ class AvailabilityIndicator extends StatelessWidget {
           Container(
             width: 12,
             height: 26,
-            decoration: const BoxDecoration(
-              color: CupertinoColors.activeGreen,
+            decoration: BoxDecoration(
+              color: circleColor,
               shape: BoxShape.circle,
             ),
           ),
-          const SizedBox(width: 8),
+          SizedBox(width: 8),
           Text(
-            'Available',
+            statusText,
             style: CupertinoTheme.of(context).textTheme.textStyle.copyWith(
                 color: CupertinoColors.label,
                 fontSize: 14,
@@ -106,6 +125,20 @@ class AvailabilityIndicator extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class CombinedTexts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        InfoText(),
+        const SizedBox(width: 8),
+        RatingText(),
+      ],
     );
   }
 }
@@ -120,9 +153,7 @@ class InfoRow extends StatelessWidget {
         children: [
           IconPlaceholder(),
           const SizedBox(width: 8),
-          Expanded(child: InfoText()),
-          const SizedBox(width: 8),
-          RatingText(),
+          Expanded(child: CombinedTexts()),
         ],
       ),
     );
@@ -168,41 +199,133 @@ class InfoText extends StatelessWidget {
 class RatingText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      '5.0 Rating',
-      style: TextStyle(
-        color: CupertinoColors.black.withOpacity(0.6),
-        fontSize: 12,
-        fontFamily: 'Inter',
-        fontWeight: FontWeight.w500,
-        letterSpacing: -0.48,
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          FeatherIcons.star,
+          color: CupertinoColors.systemYellow,
+          size: 16,
+        ),
+        const SizedBox(width: 2),
+        Text(
+          '5.0',
+          style: TextStyle(
+            color: CupertinoColors.black.withOpacity(0.6),
+            fontSize: 12,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w500,
+            letterSpacing: -0.48,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class InfoCardsRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: buildInfoCard(
+              icon: FeatherIcons.clock,
+              title: 'Expiration Date',
+              subtitle: '2023-10-21',
+              context: context,
+            ),
+          ),
+          SizedBox(width: 16),
+          Expanded(
+            child: buildInfoCard(
+              icon: FeatherIcons.mapPin,
+              title: 'Pickup Time',
+              subtitle: 'Today, 12:03pm',
+              context: context,
+            ),
+          ),
+          SizedBox(width: 16),
+          Expanded(
+            child: buildInfoCard(
+              icon: FeatherIcons.alertCircle,
+              title: 'Allergens',
+              subtitle: 'See below',
+              context: context,
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget buildInfoCard({
+    required IconData icon,
+    required String title,
+    required String subtitle,
+    required BuildContext context,
+  }) {
+    return ConstrainedBox(
+        constraints: BoxConstraints(
+          minWidth: 100,
+          maxWidth: 150,
+          minHeight: 100,
+          maxHeight: 150,
+        ),
+        child: CupertinoCard(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(icon, color: CupertinoColors.systemGrey),
+              SizedBox(height: 8.0),
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 10,
+                  color: CupertinoColors.label.resolveFrom(context),
+                ),
+              ),
+              SizedBox(height: 4.0),
+              Text(
+                subtitle,
+                style: TextStyle(
+                  fontSize: 8,
+                  color: CupertinoColors.systemGrey.resolveFrom(context),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
 
 class PickupInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Pickup Information',
-            style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
-          ),
-          const SizedBox(height: 8),
-          const Text('Today, 12:03pm'),
-          const SizedBox(height: 16),
-          Text(
-            'Meeting Point',
-            style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
-          ),
-          const SizedBox(height: 8),
-          const Text('330, 1130 Trello Way\nKelowna, BC\nV1V 5E0'),
-        ],
+    return CupertinoCard(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Pickup Information',
+              style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
+            ),
+            const SizedBox(height: 8),
+            const Text('Today, 12:03pm'),
+            const SizedBox(height: 16),
+            Text(
+              'Meeting Point',
+              style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
+            ),
+            const SizedBox(height: 8),
+            const Text('330, 1130 Trello Way\nKelowna, BC\nV1V 5E0'),
+          ],
+        ),
       ),
     );
   }
@@ -211,35 +334,74 @@ class PickupInformation extends StatelessWidget {
 class AllergensSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Allergens',
-            style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
-          ),
-          const SizedBox(height: 8),
-          const Text(
-              'Peanuts, Tree nuts (e.g., almonds, cashews, walnuts), Milk, Eggs, Soy'),
-        ],
+    return CupertinoCard(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Allergens',
+              style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
+            ),
+            const SizedBox(height: 8),
+            const Text(
+                'Peanuts, Tree nuts (e.g., almonds, cashews, walnuts), Milk, Eggs, Soy'),
+          ],
+        ),
       ),
     );
   }
 }
 
 class ReserveButton extends StatelessWidget {
+  final bool isReserved;
+  ReserveButton({required this.isReserved});
   @override
   Widget build(BuildContext context) {
+    Color indicatorColor =
+        isReserved ? CupertinoColors.systemGrey : CupertinoColors.activeBlue;
+    Color textColor =
+        isReserved ? CupertinoColors.white : CupertinoColors.white;
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: CupertinoButton.filled(
-        child: const Text('Reserve'),
-        onPressed: () {
-          // Implement reservation logic
-        },
+      child: CupertinoButton(
+        color: indicatorColor,
+        child: Text('Reserve', style: TextStyle(color: textColor)),
+        onPressed: isReserved
+            ? null
+            : () {
+                // Add reserving logic here
+              },
+        borderRadius: BorderRadius.circular(8.0),
       ),
+    );
+  }
+}
+
+class CupertinoCard extends StatelessWidget {
+  final Widget child;
+
+  const CupertinoCard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: CupertinoColors.systemGrey.withOpacity(0.5),
+            blurRadius: 5.0,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: child,
     );
   }
 }
