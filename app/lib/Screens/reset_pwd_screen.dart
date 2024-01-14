6116e2aebca0e4500c391e4ad071be672d853c47
Ishita Gupta <ishita1809@gmail.com>
2024-01-14 22:10:55 +0400

    Added the reset/forgot password screen's UI

---
+++ app/lib/Screens/reset_pwd_screen.dart
@@ -0,0 +1,112 @@
+// forgot_pwd_screen.dart
+// A page that allows a user to reset their password if they forgot it.
+
+import 'package:flutter/cupertino.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  @override
+  _ForgotPasswordState createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPasswordScreen> {
+
+  // Placeholder function for password reset logic
+  Future<void> _resetPassword() async {
+    // Implement your password reset logic here
+    // For example, you can send a password reset email to the user
+    print('Password reset logic goes here');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemGroupedBackground,
+      child: CustomScrollView(
+        slivers: <Widget>[
+          CupertinoSliverNavigationBar(
+            backgroundColor: CupertinoColors.systemGroupedBackground,
+            largeTitle: const Text(
+              'Reset Password',
+              style: TextStyle(
+                letterSpacing: -1.36,
+              ),
+            ),
+
+            leading: CupertinoButton(
+              padding: EdgeInsets.zero,
+              child: const Icon(CupertinoIcons.arrow_left_circle_fill, color: Color.fromRGBO(51, 117, 134, 1.0), size: 30.0,),
+              onPressed: () async{
+                Navigator.of(context).pop();
+              }
+            ),
+
+            border: const Border(bottom: BorderSide.none),
+            stretch: true,
+          ),
+          
+          SliverToBoxAdapter(
+            child: SizedBox(height: 16.0),
+          ),
+
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Please enter the email address associated with your account.', // Add your content here
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          ),
+
+          SliverToBoxAdapter(
+            child: SizedBox(height: 16.0),
+          ),
+
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(left: 17.0, top: 5.0, right: 17.0), // Adjust outer padding as needed
+              child: CupertinoTextField(
+                padding: EdgeInsets.all(16.0),
+                placeholder: "Email Address",
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: CupertinoColors.secondarySystemGroupedBackground,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: CupertinoColors.secondarySystemGroupedBackground,
+                ),
+              ),
+            ),
+          ),
+
+          SliverToBoxAdapter(
+            child: SizedBox(height: 64.0),
+          ),
+
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: CupertinoButton(
+                color: Color.fromRGBO(51, 117, 134, 1.0),
+                borderRadius: BorderRadius.circular(10),
+                minSize: 44,
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                onPressed: _resetPassword,
+                child: Text(
+                  'Submit',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: -0.8,
+                    color: CupertinoColors.secondarySystemGroupedBackground,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );       
+  }
+}
