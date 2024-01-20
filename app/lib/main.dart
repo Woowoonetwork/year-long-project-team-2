6f0baea939317d7d79bc31cfdbfe091d6e8c54dc
Nick <1049379870@qq.com>
2024-01-20 14:25:25 -0800

    login hotfix

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,6 @@
+// main.dart
+// entry point of the app
+
 import 'package:FoodHood/Screens/home_screen.dart';
 import 'package:FoodHood/Screens/login_screen.dart';
 import 'package:FoodHood/Screens/navigation_screen.dart';
@@ -6,22 +9,27 @@ import 'package:FoodHood/Screens/welcome_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter/material.dart';
 import 'package:FoodHood/auth_wrapper.dart';
+import 'auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
-import 'firestore_service.dart';
-import 'auth_service.dart';
+import 'package:FoodHood/firestore_service.dart';
 
 void main() async {
+  // Initialize Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  // Call the function to add a pre-defined list of allergens and categories
   await addAllergensCategoriesAndPL();
 
+  // Run the app
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]).then((_) {
+    DeviceOrientation.portraitDown
+  ]) // Restrict orientation to portrait
+      .then((_) {
     runApp(FoodHoodApp());
   });
 }
@@ -30,32 +38,48 @@ class FoodHoodApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: AuthWrapper(),
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: _generateRoute,
-    );
-  }
+      localizationsDelegates: [
+        DefaultMaterialLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
+      home: AuthWrapper(), // Use AuthWrapper as the root widget
+      debugShowCheckedModeBanner:
+          false, // Hide the debug banner in Preview mode
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return CupertinoPageRoute(
+              builder: (context) => WelcomeScreen(), // Root route
+            );
 
-  Route<dynamic> _generateRoute(RouteSettings settings) {
-    final Map<String, WidgetBuilder> routes = {
-      '/': (context) => WelcomeScreen(),
-      '/signup': (context) =>
-          RegistrationScreen(auth: AuthService(FirebaseAuth.instance)),
-      '/signin': (context) => LogInScreen(),
-      '/home': (context) => HomeScreen(),
-      '/nav': (context) => NavigationScreen(
-            selectedIndex:
-                ModalRoute.of(context)?.settings.arguments as int? ?? 0,
-            onItemTapped: (index) {},
-          ),
-    };
-
-    // Provide a default route in case the specified route name is not found
-    WidgetBuilder builder = routes[settings.name] ?? (context) => HomeScreen();
-
-    return CupertinoPageRoute(
-      builder: builder,
-      settings: settings,
+          case '/signup':
+            return CupertinoPageRoute(
+              builder: (context) => RegistrationScreen(
+                  auth: AuthService(FirebaseAuth.instance)), // Signup route
+            );
+          case '/signin':
+            return CupertinoPageRoute(
+              builder: (context) => LogInScreen(),
+            );
+          case '/home':
+            return CupertinoPageRoute(
+              builder: (context) => HomeScreen(),
+            );
+          case '/nav':
+            return CupertinoPageRoute(
+              builder: (context) => NavigationScreen(
+                selectedIndex:
+                    ModalRoute.of(context)?.settings.arguments as int? ?? 0,
+                onItemTapped: (index) {},
+              ),
+            );
+          default:
+            return CupertinoPageRoute(
+              builder: (context) => HomeScreen(),
+            );
+        }
+      },
     );
   }
 }
