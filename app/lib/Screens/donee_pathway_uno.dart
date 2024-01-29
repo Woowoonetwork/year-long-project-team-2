1827255ffa7391a68dd9f22e04522df2592e734d
Jayati <jae186@gmail.com>
2024-01-28 23:41:59 -0800

    fixing up page

--- app/lib/Screens/donee_pathway_uno.dart
+++ app/lib/Screens/donee_pathway_uno.dart
@@ -1,3 +1,4 @@
+import 'package:FoodHood/Components/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,66 +11,104 @@ class _DoneePathState extends State<DoneePath> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      backgroundColor: backgroundColor,
       navigationBar: CupertinoNavigationBar(
+        backgroundColor: backgroundColor,
         leading: CupertinoNavigationBarBackButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
-        middle: Text('Reservation'),
         trailing: CupertinoButton(
           padding: EdgeInsets.zero,
           onPressed: () {},
-          child: Text('Message first name of donor'),
+          child: Text('Message Harry'),
         ),
+        border: null,
       ),
       child: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'You have reserved the from first name of donor',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.asset(
-                  'path/to/your/image.jpg',
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Positioned.fill(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'Posted by  2 mins ago',
-                    style: TextStyle(color: Colors.grey),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Text(
+                      'You have reserved the Chicken and Rice from Harry',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 34,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: -1.70,
+                      ),
+                    ),
                   ),
-                  Icon(
-                    CupertinoIcons.star_fill,
-                    color: Colors.amber,
-                  ),
-                  Text('')
+                  Padding(
+                      padding: EdgeInsets.only(top: 24.0),
+                      child: Image(
+                          image:
+                              AssetImage('assets/images/sampleFoodPic.png'))),
                 ],
               ),
             ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
+            Positioned(
+              left: 24,
+              right: 24,
+              bottom: 50,
+              child: CupertinoButton.filled(
+                onPressed: () {},
+                child: Text('Cancel Reservation'),
+              ),
+            ),
+            Positioned(
+              left: 24,
+              right: 24,
+              bottom: 120,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF9FD0C6),
+                  borderRadius: BorderRadius.circular(100),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0x19000000),
+                      blurRadius: 10,
+                      offset: Offset(0, 0),
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
                 child: Padding(
-                  padding: const EdgeInsets.only(bottom: 50.0),
-                  child: CupertinoButton.filled(
-                    child: Text('Cancel Reservation'),
-                    onPressed: () {},
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 94, vertical: 18),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Pending Confirmation',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: -0.90,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        '2 mins left',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: -0.90,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
