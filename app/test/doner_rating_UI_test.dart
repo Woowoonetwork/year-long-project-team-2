58e1cbd7c53776ca266e9f2b3ec92565990e270f
Dhruv Bihani <dhruvbihani@gmail.com>
2024-01-28 22:19:01 -0800

    added test for doner and donee UI rating

---
+++ app/test/doner_rating_UI_test.dart
@@ -0,0 +1,28 @@
+import 'package:FoodHood/Screens/donor_rating.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:FoodHood/Screens/home_screen.dart';
+import 'mock.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    setupFirebaseAnalyticsMocks();
+    if (Firebase.apps.isEmpty) {
+      await Firebase.initializeApp();
+    }
+  });
+  testWidgets('Check for search bar, buttons, and list in MyWidget',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(CupertinoApp(
+      home: DonorRatingPage(
+        postId: '0423e12c-792f-4986-a723-c701f3cf5332',
+      ),
+    ));
+    expect(find.byType(CupertinoTextField), findsOneWidget);
+    expect(find.byType(CupertinoButton), findsWidgets);
+    expect(find.byType(Icon), findsWidgets);
+  });
+}
