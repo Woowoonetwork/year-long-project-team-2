891d1e1205e0cf418c075a9c367eaf5a0ce80f5c
Jayati <jae186@gmail.com>
2024-03-03 22:47:00 -0800

    tried to add animation

--- app/lib/Components/slimProgressBar.dart
+++ app/lib/Components/slimProgressBar.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:FoodHood/Components/colors.dart';
 
-class SlimProgressBar extends StatelessWidget {
+class SlimProgressBar extends StatefulWidget {
   final List<String> stepTitles;
   final String postStatus;
 
   SlimProgressBar({required this.stepTitles, required this.postStatus});
 
   @override
-  Widget build(BuildContext context) {
-    int currentIndex = _getCurrentIndex();
-    int totalSteps = stepTitles.length;
+  _SlimProgressBarState createState() => _SlimProgressBarState();
+}
 
+class _SlimProgressBarState extends State<SlimProgressBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = _getCurrentIndex();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _animation = Tween<double>(begin: 0, end: _currentIndex.toDouble())
+        .animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void didUpdateWidget(covariant SlimProgressBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    int newIndex = _getCurrentIndex();
+    if (_currentIndex != newIndex) {
+      _currentIndex = newIndex;
+      _animation =
+          Tween<double>(begin: _animation.value, end: _currentIndex.toDouble())
+              .animate(_animationController);
+      _animationController.forward(from: 0);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       height: 100,
       padding: EdgeInsets.symmetric(horizontal: 16),
@@ -21,34 +52,41 @@ class SlimProgressBar extends StatelessWidget {
             height: 50,
             child: Container(
               width: double.infinity,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 2,
-                    color: Color.fromARGB(255, 133, 210, 175),
-                  ),
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: List.generate(totalSteps, (index) {
-                        return _buildStepIndicator(index, currentIndex);
-                      }),
-                    ),
-                  ),
-                ],
+              child: AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, child) {
+                  return Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: 2,
+                        color: Color.fromARGB(255, 133, 210, 175),
+                      ),
+                      Positioned(
+                        left: 0,
+                        right: 0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children:
+                              List.generate(widget.stepTitles.length, (index) {
+                            return _buildStepIndicator(
+                                index, _animation.value.toInt());
+                          }),
+                        ),
+                      ),
+                    ],
+                  );
+                },
               ),
             ),
           ),
           SizedBox(height: 8),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: List.generate(totalSteps, (index) {
+            children: List.generate(widget.stepTitles.length, (index) {
               return Text(
-                stepTitles[index],
+                widget.stepTitles[index],
                 style: TextStyle(
                   fontSize: 12,
                   color: Colors.grey[600],
@@ -61,8 +99,8 @@ class SlimProgressBar extends StatelessWidget {
     );
   }
 
-  Widget _buildStepIndicator(int index, int currentIndex) {
-    bool isActive = index <= currentIndex;
+  Widget _buildStepIndicator(int index, int animatedIndex) {
+    bool isActive = index <= animatedIndex;
     return Container(
       width: 10,
       height: 10,
@@ -75,7 +113,7 @@ class SlimProgressBar extends StatelessWidget {
   }
 
   int _getCurrentIndex() {
-    switch (postStatus) {
+    switch (widget.postStatus) {
       case 'confirmed':
         return 0;
       case 'out_for_delivery':
@@ -87,5 +125,11 @@ class SlimProgressBar extends StatelessWidget {
       default:
         return 0;
     }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
